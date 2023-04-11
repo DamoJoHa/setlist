595cea231a202de97b5164ccb2d64a8494b7aec6
Damian Hallengren <djhallengren@gmail.com>
2023-04-11 16:46:53 +0200

    Created events controller with view, and simmple seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.new
+user.username = "owner"
+user.email = "owner@test.mail"
+user.password = "password"
+user.save
+
+artist = User.new
+artist.username = "artist"
+artist.email = "artist@test.mail"
+artist.password = "password"
+artist.save
+
+act = Act.new
+act.name = "Blazing Glory"
+act.description = "A new group with an old sound, playing all the songs you love."
+act.genre = "Rock"
+act.user = artist
+act.save
+
+venue = Venue.new
+venue.address = "Somewhere"
+venue.description = "A real dump."
+venue.name = "The Chum Bucket"
+venue.category = "restaurant"
+venue.user = user
+venue.save
+
+event = Event.new
+event.start_time = Time.now
+event.end_time = Time.now + 100
+event.description = "Blazing GLory at the Chum Bucket"
+event.act = act
+event.venue = venue
+event.save

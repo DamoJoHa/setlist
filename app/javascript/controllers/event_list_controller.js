8812dfddab27ca036be37c554445f64a52483b35
Damian Hallengren <djhallengren@gmail.com>
2023-07-26 18:37:02 +0200

    Now correctly removes highlights on index page

--- app/javascript/controllers/event_list_controller.js
+++ app/javascript/controllers/event_list_controller.js
@@ -5,10 +5,16 @@ export default class extends Controller {
   static targets = ["list"]
 
   connect() {
+    // console.log("connected event list")
   }
 
   highlight(event) {
     // remove highlight from all items
+    const listItems = Array.from(this.listTarget.children)
+    // console.log(listItems);
+    listItems.forEach(item => {
+      item.classList.remove("green-shadow")
+    })
 
     //add highlight to clicked item
     console.log("highlighting")
